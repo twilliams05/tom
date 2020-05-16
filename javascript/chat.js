ba4b0ec6e288bd237bf24dc9bc56e485137d64f1
twilliams05 <tomawilliams2005@gmail.com>
2020-05-16 19:07:16 +0100

    trying to style chat + styling pics header

--- javascript/chat.js
+++ javascript/chat.js
@@ -5,7 +5,7 @@ const chatButton = $("#chat-button");
 const chatForm = $("#chat-form");
 
 const chatFn = (e) => {
-   chatMessagesContainer.append("<p>" + chatInput.val() + " (sent at " + moment().format("HH:mm") + ")</p>");
+   chatMessagesContainer.append("<p>" + "User123 : " + chatInput.val() + " (sent at " + moment().format("HH:mm") + ")</p>");
    chatInput.val("");
    chatInput.focus();
   e.preventDefault();
